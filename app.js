b36288e41cada524ae6f33bd0c375d5314b25965
ai-small <aismall@yandex.ru>
2020-09-13 05:47:34 +1200

    added crash-test

--- app.js
+++ app.js
@@ -39,6 +39,12 @@ app.use(cookieParser());
 
 app.use(requestLogger);
 
+app.get('/crash-test', () => {
+  setTimeout(() => {
+    throw new Error('Сервер сейчас упадёт');
+  }, 0);
+});
+
 app.use('/signin', signInRouter);
 app.use('/signup', signUpRouter);
 
